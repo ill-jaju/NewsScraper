8720a1e8f2baa9b4af7b3f5a38bd5784ae6f12ac
jamie-jessi <jamiejudi@gmail.com>
2017-09-17 22:04:19 -0500

    updated index.js with folders

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'); //dependencies
 var exphbs = require("express-handlebars");
 var express = require('express');
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
-var port = process.env.PORT || 8000;
+mongoose.Promise = Promise; //set mongoose to leverage built in js es6 promises
 
+var port = process.env.PORT || 8000; //express init
 var app = express();
 
-app.use(bodyParser.json()); //bodyParser next four lines - standard copy/paste
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(logger("dev")); //morgan + body parser with app
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(express.static('public')); //make public a static directory
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" })); //set handlebars
 app.set("view engine", "handlebars");
 
-app.get('/', function (req, res) {
-  res.render('home');
+//mongoose db config
+var databaseUri = 'mongodb://localhost/'; //local dir
+var MONGODB_URI = 'mongodb://heroku_gn16lnt5:c9k37d228nl33tk4k49b6fdh1k@ds139964.mlab.com:39964/heroku_gn16lnt5'; //heroku config
+
+if (process.env.MONGODB_URI) { //if being run in app
+  mongoose.connect(process.env.MONGODB_URI);
+} else { //if on local machine
+  mongoose.connect(databaseUri);
+}
+//db config end
+
+var db = mongoose.connection;
+
+db.on('error', function(error) { //displays errors
+  console.log('mongoose error: ', error);
 });
+
+db.once('open', function() { //console log success if connected to mongoose 
+  console.log('mongoose connection successful');
+});
+
+var routes = require('./controllers'); //routes
+app.use('/', routes);
 
 app.listen(port, function(){ //makes sure app is working
   console.log("app on " + port);
