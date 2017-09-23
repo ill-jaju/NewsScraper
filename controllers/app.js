43c2735e32e60660f9dc0f730986919dd9509e45
jamie-jessi <jamiejudi@gmail.com>
2017-09-23 00:50:14 -0500

    scrape saved to db and sent to /articles as json

--- controllers/app.js
+++ controllers/app.js
@@ -11,32 +11,54 @@ router.get('/', function (req, res) {
   res.render('index');
 })
 
-//scrape data
-request("https://arstechnica.com/", function(error, response, html) {
+router.get('/scrape', function(req, res){
+  //scrape data
+  request("https://arstechnica.com/", function(error, response, html) {
 
-  var $ = cheerio.load(html); // load the HTML into cheerio and save it to a variable
-  
-  var results = []; // An empty array to save the data that we'll scrape
+    var $ = cheerio.load(html); // load the HTML into cheerio and save it to a variable
+    
+    var result = {}; // An empty array to save the data that we'll scrape
 
-  $("article.type-report").each(function(i, element) {
+    $("article.type-report").each(function(i, element) {
 
-    var title = $(element).find("h2").text();
-    var link = $(element).find("h2").find("a").attr("href");
-    var author = $(element).find(".byline").find("a").text();
-    var summary = $(element).find(".excerpt").text().slice(0, 200);
-    // var picture = $(element).find(".listing-small").css("background-image:url").text()
+      result.title = $(element).find("h2").text();
+      result.link = $(element).find("h2").find("a").attr("href");
+      result.author = $(element).find(".byline").find("a").text();
+      result.summary = $(element).find(".excerpt").text().slice(0, 200);
+      // var picture = $(element).find(".listing-small").css("background-image:url").text()
 
-    results.push({ // save these results in an object that we'll push into the results array we defined earlier
-      title: title,
-      link: link,
-      author: author,
-      summary: summary,
-      // picture: picture
+      // results.push({ // save these results in an object that we'll push into the results array we defined earlier
+      //   title: title,
+      //   link: link,
+      //   author: author,
+      //   summary: summary,
+      //   // picture: picture
+      // });
+      var entry = new Article(result); //creates new entry using Article model from import, passes the entries from scrape 
+      
+      entry.save(function(err, data) { //saves entry to database
+        if (err) {
+          console.log('scrape ' + err);
+        } else {
+          console.log(data);
+        }
+      });
+      res.send("Scrape Complete");
     });
-  });
 
-  // Log the results once you've looped through each of the elements found with cheerio
-  console.log(results);
+    // Log the results once you've looped through each of the elements found with cheerio
+    // console.log(results);
+  });
+})
+
+router.get('/articles', function(req, res) {
+  Article.find({}, function(err, data) {
+    if (err) {
+      console.log ('article ' + err);
+    } else {
+      res.json(data);
+    }
+  });
 });
 
 module.exports = router;
